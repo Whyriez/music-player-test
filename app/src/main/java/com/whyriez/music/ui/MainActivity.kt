db14229e184171d34f1296d74e047072ddde9410
whyriez <sumaalim@gmail.com>
2026-09-05 16:49:02 +0800

    feat(ui): add search bar, song recycler view, and bottom player bar

--- app/src/main/java/com/whyriez/music/ui/MainActivity.kt
+++ app/src/main/java/com/whyriez/music/ui/MainActivity.kt
@@ -10,7 +10,7 @@ import com.whyriez.music.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
